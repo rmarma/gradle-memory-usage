d5e576cbf4037997501577950745ca8941d5dbb8
rma <rommaks91@yandex.ru>
2024-09-14 13:45:07 +0300

    add some task with large input

--- java-module-01/build.gradle.kts
+++ java-module-01/build.gradle.kts
@@ -1,3 +1,19 @@
 plugins {
     java
 }
+
+class SomeLargeObject(
+    val data: ByteArray = ByteArray(size = 100 * 1024 * 1024) // 100 Mb
+) : java.io.Serializable
+
+tasks.register("someTask") {
+    inputs.property("largeObject", SomeLargeObject())
+    inputs.property("duration", 1_000L) // 1 sec.
+    doLast {
+        val largeObject: SomeLargeObject = inputs.properties["largeObject"] as SomeLargeObject
+        val duration: Long = inputs.properties["duration"] as Long
+        logger.lifecycle("$path start - $largeObject")
+        Thread.sleep(duration)
+        logger.lifecycle("$path end - $largeObject")
+    }
+}
